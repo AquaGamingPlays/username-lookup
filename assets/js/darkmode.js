2c024b2c9b0c4a565f2ab8690ecd03eac35606eb
un <70737623+unpredictable@users.noreply.github.com>
2020-11-18 22:46:57 -0500

    misc dark mode changes

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -38,25 +38,25 @@ function darkMode_newHTML() {
     }
     try {
         var y = document.getElementById("username");
-        y.classList.toggle("dm1")
+        y.classList.toggle("list-dm")
     } catch (err) {
         console.log("error toggling dark mode for a class")
     }
     try {
         var y = document.getElementById("status");
-        y.classList.toggle("dm1")
+        y.classList.toggle("list-dm")
     } catch (err) {
         console.log("error toggling dark mode for a class")
     }
     try {
         var y = document.getElementById("uuid");
-        y.classList.toggle("dm1")
+        y.classList.toggle("list-dm")
     } catch (err) {
         console.log("error toggling dark mode for a class")
     }
     try {
         var y = document.getElementById("history");
-        y.classList.toggle("dm1")
+        y.classList.toggle("list-dm")
     } catch (err) {
         console.log("error toggling dark mode for a class")
     }
@@ -68,13 +68,13 @@ function darkMode_newHTML() {
     }
     try {
         var y = document.getElementById("dark7");
-        y.classList.toggle("dm1")
+        y.classList.toggle("list-dm")
     } catch (err) {
         console.log("error toggling dark mode for a class")
     }
     try {
         var y = document.getElementById("dark8");
-        y.classList.toggle("dm1")
+        y.classList.toggle("list-dm")
     } catch (err) {
         console.log("error toggling dark mode for a class")
     }
