36f8b410c0e83204a7dce151bf7d7cc7126769c7
un <70737623+unpredictable@users.noreply.github.com>
2020-11-19 17:21:36 -0500

    Fix #8

--- assets/js/phd.js
+++ assets/js/phd.js
@@ -2,16 +2,16 @@
 // copyright (c) tanpug 2020
 
 // user info stuff
-if (query == undefined) {} else {
-    removeElement('alertbox'); // get rid of the alert
-    new_html = '<div class="container" style="text-align:center"><div class="row" style="text-align:center"><div id="userbox" class="col-sm-4"><h3>User Info</h3><ul id="dark" class="list-group" style="text-align:center"><li id="username" class="list-group-item"></li><li id="status" class="list-group-item"></li><li id="uuid" class="list-group-item"><code id="uuidcode dark9"></code></li><li id="history" class="list-group-item"></li></ul></div><div id="capebox" class="col-sm-4"><h3>Cape Info</h3><ul id="dark6" class="list-group" style="text-align:center"><li id="dark8" class="list-group-item">OptiFine Cape<br/><img id="optifine" width="92" height="44"></li></ul></div></div></div>';
-    // ^^^^^^ add new html for the actual skin viewer, user info, cape info etc
-    document.getElementById('bodybox').innerHTML += new_html; // add the new html
-    var darkmodetoggle = localStorage.getItem('darkmode'); // check if we have dark mode toggled
-    if (darkmodetoggle === "true") {
-        darkMode_newHTML(); // we need to toggle dark mode for the new HTML separately since we're injecting it after main.js has loaded.
-    }
+if (query == undefined) {} else { // make sure we don't show empty info boxes unless we have a real query
     $.get("https://phdcheck.mcapi.workers.dev/uuid/" + query).done(function(data) {
+        removeElement('alertbox'); // get rid of the alert
+        new_html = '<div class="container" style="text-align:center"><div class="row" style="text-align:center"><div id="userbox" class="col-sm-4"><h3>User Info</h3><ul id="dark" class="list-group" style="text-align:center"><li id="username" class="list-group-item"></li><li id="status" class="list-group-item"></li><li id="uuid" class="list-group-item"><code id="uuidcode dark9"></code></li><li id="history" class="list-group-item"></li></ul></div><div id="capebox" class="col-sm-4"><h3>Cape Info</h3><ul id="dark6" class="list-group" style="text-align:center"><li id="dark8" class="list-group-item">OptiFine Cape<br/><img id="optifine" width="92" height="44"></li></ul></div></div></div>';
+        // ^^^^^^ add new html for the actual skin viewer, user info, cape info etc
+        document.getElementById('bodybox').innerHTML += new_html; // add the new html
+        var darkmodetoggle = localStorage.getItem('darkmode'); // check if we have dark mode toggled
+        if (darkmodetoggle === "true") {
+            darkMode_newHTML(); // we need to toggle dark mode for the new HTML separately since we're injecting it after main.js has loaded.
+        }
         // initialize variables
         var uuid = data.uuid; // account uuid
         var username = data.username; // username
@@ -74,5 +74,21 @@ if (query == undefined) {} else {
                 document.getElementById('dark11').style.display = '';
             }
         });
+    }).fail(function(data) {
+        if (data.responseJSON.error == "does_not_exist") { // UUID never existed / hard-deleted
+            var alert_mode = "alert-danger";
+            var uuid_status = '<p>The UUID <code>' + query + '</code> was never tied to a Minecraft profile, or the Minecraft profile tied to the UUID has since been hard-deleted.</p>'; // if uuid doesn't exist
+        } else if (data.responseJSON.error == "taken") { // UUID is taken already
+            var alert_mode = "alert-info";
+            var uuid_status = '<p>The UUID <code>' + query + '</code> is already tied to a Minecraft profile! Visit the <a href="./index.html?lookup=' + query + '">info page</a> to find out which Minecraft profile has this UUID!</p>'; // if uuid is taken
+        } else if (data.responseJSON.error == "invalid_uuid") { // UUID isn't valid format
+            var alert_mode = "alert-warning";
+            var uuid_status = '<p>The UUID <code>' + query + '</code> isn\'t the right format. Did you make sure it\'s a valid UUIDv4?</p>'; // if uuid is invalid
+        }
+        // replace the alert info with new content based on what error we encountered
+        var w = document.getElementById("alertbox");
+        w.classList.toggle("alert-secondary");
+        w.classList.toggle(alert_mode);
+        document.getElementById('alertbox').innerHTML = '<h4 class="alert-heading"><code>' + query + '</code></h4>' + uuid_status;
     });
 }
